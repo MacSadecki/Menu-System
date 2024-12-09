3bcddf0c66b96cff163da594311dd5d883c2f488
Maciej Sadecki <mac.sadecki@gmail.com>
2024-12-09 16:37:30 +0100

    Add Color Chaning
    Added the interaction for changing the color of the UI element

--- Assets/Scripts/Interactions/InteractionChangeColor.cs
+++ Assets/Scripts/Interactions/InteractionChangeColor.cs
@@ -1,16 +1,27 @@
 using UnityEngine;
+using UnityEngine.UI;
 
-public class InteractionChangeColor : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+public class InteractionChangeColor : MonoBehaviour, IInteractable
+{    
+    public string interactionButtonText = "Change color";
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField]
+    private Color[] colors;
+    private int currentColorIndex = 0;
+
+    public void Interact(GameObject gameObject)
     {
+        // Check if colors array has something in it, otherwise there would not be any colors to pick from
+        if (colors.Length == 0)
+        {
+            Debug.LogError("The colors array is empty, please add at least one color to the array");
+            return;
+        }
+        // Check if currentColorIndex is not exceeding the array size so the whole thing would not explode
+        if (currentColorIndex >= colors.Length) currentColorIndex = 0;
+
+        gameObject.GetComponent<Image>().color = colors[currentColorIndex];        
         
+        currentColorIndex ++;
     }
 }
